991061a85385c1cd043aed9459cfc16fe6239aa8
LeeCenY <634348197@qq.com>
2018-06-19 23:13:39 +0800

    新增API：修改住户信息和七牛图片URL

--- Sources/iRent/Handlers/ExpiredRent.swift
+++ Sources/iRent/Handlers/ExpiredRent.swift
@@ -34,7 +34,6 @@ public class ExpiredRent: BaseHandler {
                     return
                 }
                 
-                
                 //水表数
                 guard let water: Int = dict["water"] as? Int else {
                     resError(request, response, error: "水表数 water 请求参数不正确")
@@ -45,41 +44,124 @@ public class ExpiredRent: BaseHandler {
                     resError(request, response, error: "电表数 electricity 请求参数不正确")
                     return
                 }
-                //租金
-                guard let rent_money: Int = dict["rent_money"] as? Int else {
-                    resError(request, response, error: "租金 rent_money 请求参数不正确")
-                    return
-                }
-                //网络
-                guard let network: Int = dict["network"] as? Int else {
-                    resError(request, response, error: "网络 network 请求参数不正确")
-                    return
-                }
-                //垃圾费
-                guard let trash_fee: Int = dict["trash_fee"] as? Int else {
-                    resError(request, response, error: "网络 network 请求参数不正确")
-                    return
-                }
-
-                let roomTable = db.table(Room.self)
+                
+                let roomTable = db().table(Room.self)
                 let query = roomTable.where(\Room.id == id)
                 
                 guard try query.count() != 0 else {
                     resError(request, response, error: "房间 id 不存在")
                     return
                 }
-
+                
+                var rent_money = Int()
+                var network = Int()
+                var trash_fee = Int()
+                
+                for row in try query.select() {
+                    rent_money = row.rent_money!
+                    network = row.network!
+                    trash_fee = row.trash_fee!
+                }
+                
+                let paymentTable = db().table(Payment.self)
+                let queryCount = try paymentTable
+                    .where(\Payment.room_id == id && \Payment.rent_date == rentDate)
+                    .count()
+                
+                //先 insert 本月信息
+                if queryCount == 0  {
+                    let payment = Payment.init(id: UUID(), room_id: id, state: false, payee: nil, rent_date: rentDate, money: nil, rent_money: rent_money, water: nil, electricity: nil, network: network, trash_fee: trash_fee, image_url: nil, arrears: nil, remark: nil, create_at: Date().iso8601(), updated_at: Date().iso8601())
+                    
+                    try paymentTable.insert(payment)
+                }
+                
+                
                 //TODO 还需要判断时间
                 
-                let paymentTable = db.table(Payment.self)
+                let payment = Payment.init(id: UUID(), room_id: UUID(), state: false, payee: nil, rent_date: rentDate, money: nil, rent_money: rent_money, water: water, electricity: electricity, network: network, trash_fee:trash_fee, image_url: nil, arrears: nil, remark: nil, create_at: Date().iso8601(), updated_at: Date().iso8601())
                 
-                let payment = Payment.init(id: UUID(), room_id: UUID(), state: false, payee: nil, rent_date: rentDate, money: nil, rent_money: rent_money, water: water, electricity: electricity, network: network, trash_fee:trash_fee, arrears: nil, remark: nil, create_at: Date().iso8601(), updated_at: Date().iso8601())
-
+                
                 try paymentTable
                     .where(\Payment.room_id == id && \Payment.rent_date == rentDate)
                     .update(payment, ignoreKeys: \.id, \.room_id, \.create_at, \.money)
                 
                 try response.setBody(json: ["success": true, "status": 200, "data": "更新成功"])
+                response.completed()
+            } catch {
+                serverErrorHandler(request, response)
+                Log.error(message: "update : \(error)")
+            }
+        }
+    }
+    
+    /// 修改
+    ///
+    /// - Parameters:
+    ///   - request: 请求
+    ///   - response: 响应
+    static func change() -> RequestHandler {
+        return {
+            request, response in
+            do {
+                guard
+                    let json = request.postBodyString,
+                    let dict = try json.jsonDecode() as? [String: Any]
+                    else {
+                        resError(request, response, error: "请填写请求参数")
+                        return
+                }
+                //id
+                guard let id = UUID.init(uuidString: (dict["id"] as? String)!) else {
+                    resError(request, response, error: "id 请求参数不正确")
+                    return
+                }
+                
+                //租金
+                guard  let rentMeony: Int = dict["rentmeony"] as? Int, rentMeony > 0 else {
+                    resError(request, response, error: "租金 rentmeony 请求参数不正确")
+                    return
+                }
+                
+                //押金
+                guard  let deposit: Int = dict["deposit"] as? Int, rentMeony > 0 else {
+                    resError(request, response, error: "押金 deposit 请求参数不正确")
+                    return
+                }
+                
+                //网络
+                guard let network: Int = dict["network"] as? Int else {
+                    resError(request, response, error: "网络 network 请求参数不正确")
+                    return
+                }
+                
+                //垃圾费
+                guard let trashfee: Int = dict["trashfee"] as? Int else {
+                    resError(request, response, error: "垃圾费 trashfee 请求参数不正确")
+                    return
+                }
+                
+                let roomTable = db().table(Room.self)
+                let query = roomTable.where(\Room.id == id && \Room.state == false)
+                
+                guard try query.count() != 0 else {
+                    resError(request, response, error: "房间 id 不存在或已经退房")
+                    return
+                }
+                
+                let room = Room.init(id: UUID(), state: false, room_no: nil, rent_money: rentMeony, deposit: deposit, lease_term: nil, rent_date: nil, network: network, trash_fee: trashfee, create_at: Date().iso8601(), updated_at: Date().iso8601(), payments: nil, tenants: nil)
+                
+                do {
+                    try roomTable
+                        .where(\Room.id == id && \Room.state == false)
+                        .update(room, setKeys: \.rent_money, \.deposit, \.network, \.trash_fee, \.updated_at)
+                } catch {
+                    try response.setBody(json: ["success": true, "status": 400, "data": "\(error)"])
+                    response.setHeader(.contentType, value: "application/json")
+                    response.completed()
+                    return
+                }
+                
+                try response.setBody(json: ["success": true, "status": 200, "data": "修改成功"])
                 response.completed()
             } catch {
                 serverErrorHandler(request, response)
--- Sources/iRent/Main/Routes.swift
+++ Sources/iRent/Main/Routes.swift
@@ -17,13 +17,17 @@ public func makeWebDemoRoutes() -> [Route] {
     routeArray.append(Route(method: .post, uri: "receive", handler: RentList.receive()))
     // 更新住户信息
     routeArray.append(Route(method: .post, uri: "update", handler: ExpiredRent.update()))
+    // 修改住户信息
+    routeArray.append(Route(method: .post, uri: "change", handler: ExpiredRent.change()))
     // 退房
     routeArray.append(Route(method: .get, uri: "checkout", handler: RentList.checkOut()))
     // 查询住户信息
     routeArray.append(Route(method: .get, uri: "roomquery", handler: RoomNo.queryRoomNo()))
     // 七牛token
     routeArray.append(Route(method: .get, uri: "qiniutoken", handler: Qiniu.qiniutoken()))
-
+    // 添加七牛图片URL
+    routeArray.append(Route(method: .post, uri: "imageurl", handler: Qiniu.qiniuImageURL()))
+    
     return routeArray
 }
 
