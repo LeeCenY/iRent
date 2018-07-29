11d01701ca84ffcd83ad467ff99b47b6e874547b
LeeCenY <634348197@qq.com>
2018-07-29 14:10:38 +0800

    完成账单状态推送消息功能

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3),
         .Package(url: "https://github.com/MatthewYork/DateTools.git", majorVersion: 4),
+        .Package(url:"https://github.com/PerfectlySoft/Perfect-Notifications.git", majorVersion: 3),
     ]
 )
 
