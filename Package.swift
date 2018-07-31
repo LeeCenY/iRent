b6f678ea60a4543a4d76637e332df629440d7bd2
LeeCenY <634348197@qq.com>
2018-07-31 15:53:34 +0800

    更新 Swift 4.2 Package 格式

--- Package.swift
+++ Package.swift
@@ -1,14 +1,24 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
     name: "iRent",
-    targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3),
-        .Package(url: "https://github.com/MatthewYork/DateTools.git", majorVersion: 4),
-        .Package(url:"https://github.com/PerfectlySoft/Perfect-Notifications.git", majorVersion: 3),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.17"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", from: "3.2.0"),
+        .package(url: "https://github.com/MatthewYork/DateTools.git", from: "4.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Notifications.git", from: "3.0.3"),
+        .package(url: "https://github.com/LeeCenY/Perfect-Qiniu.git", from: "1.0.2"),
+        ],
+    targets: [
+        .target(
+            name: "iRent",
+            dependencies: [
+                "PerfectHTTPServer",
+                "PerfectMySQL",
+                "DateToolsSwift",
+                "PerfectNotifications",
+                "PerfectQiniu"]
+        ),
     ]
 )
-
-
