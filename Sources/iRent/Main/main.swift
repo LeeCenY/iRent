294923b051e06641abbbc0faf62eb01f8e1bc2f8
LeeCenY <634348197@qq.com>
2018-08-03 16:08:07 +0800

    添加定时备份数据库

--- Sources/iRent/Main/main.swift
+++ Sources/iRent/Main/main.swift
@@ -1,12 +1,23 @@
 import PerfectLib
 import PerfectHTTP
 import PerfectHTTPServer
+import PerfectThread
+import PerfectRepeater
 
 ////初始化数据库
 try db().create(Room.self, policy: .reconcileTable)
 try db().create(Tenant.self, policy: .reconcileTable)
 try db().create(Payment.self, policy: .reconcileTable)
 try db().create(User.self, policy: .reconcileTable)
+
+
+//定时备份数据库
+Repeater.exec(timer: 86400) { () -> Bool in
+    Threading.getQueue(type: .concurrent).dispatch {
+        DBBackup().start()
+    }
+    return true
+}
 
 //API版本
 let api = "api"
